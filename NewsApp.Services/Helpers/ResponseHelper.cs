4fc49bfd70a087ef3605b09fa6b9dbe5cfe9eaf5
Vishal Rajput <01vishalpatil@gmail.com>
2025-04-28 13:47:43 +0530

    Added : Comments for Response helper

--- NewsApp.Services/Helpers/ResponseHelper.cs
+++ NewsApp.Services/Helpers/ResponseHelper.cs
@@ -4,16 +4,29 @@ using NewsApp.Models.Contracts;
 
 namespace NewsApp.Services.Helpers
 {
+    /// <summary>
+    /// A helper class to standardize HTTP responses based on API actions.
+    /// </summary>
     public class ResponseHelper
     {
+        /// <summary>
+        /// Processes a base response and returns the appropriate ActionResult.
+        /// </summary>
+        /// <param name="controller">The controller from which this is being called to build the ActionResult.</param>
+        /// <param name="baseResponse">The standardized response containing data, status, and metadata.</param>
+        /// <returns>An IActionResult generated based on the response's status code.</returns>
         public ActionResult HandleResponse(ControllerBase controller, Response baseResponse)
         {
+            // Attempt to get a CorrelationId from the request headers, if present.
             if (controller.HttpContext.Request.Headers.TryGetValue("CorrelationId", out Microsoft.Extensions.Primitives.StringValues value))
             {
                 baseResponse.CorrelationId = value;
             }
-            ;
+            // If no valid CorrelationId is retrieved, always generate a new unique identifier.
             baseResponse.CorrelationId = Guid.NewGuid().ToString();
+
+            // Return the response based on the HTTP status code using a switch expression.
+
             return baseResponse.StatusCode switch
             {
                 HttpStatusCode.OK => controller.Ok(baseResponse),
@@ -25,6 +38,12 @@ namespace NewsApp.Services.Helpers
             };
         }
 
+        /// <summary>
+        /// Creates a success response.
+        /// </summary>
+        /// <param name="data">Optional data to be included in the response.</param>
+        /// <param name="message">A descriptive message related to the success state.</param>
+        /// <returns>A Response object indicating a successful operation.</returns>
         public Response HandleSuccess(object? data = null, string? message = "")
         {
             return new Response
@@ -36,6 +55,11 @@ namespace NewsApp.Services.Helpers
             };
         }
 
+        /// <summary>
+        /// Creates a response indicating an entity was not found.
+        /// </summary>
+        /// <param name="entityName">The name of the entity that was not found.</param>
+        /// <returns>A Response object indicating a not found error.</returns>
         public Response HandleNotFound(string entityName)
         {
             return new Response
